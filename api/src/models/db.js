fbfef96c04193c03b2d68eba10f79b5882a52569
Brett Kinnamon <bdkinna@gmail.com>
2025-07-08 21:59:13 -0400

    fix: use mongoose's connect right and import model

--- api/src/models/db.js
+++ api/src/models/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
 const dbName = process.env.DB_NAME
-const dbUser = process.env.DB_USER
-const dbPass = process.env.DB_PASS
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
 
 /**
  * Connects to the database.
  */
 export async function dbConnect() {
-  const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.ga86qql.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
-  await mongoose.connect(uri);
+  const uri = `mongodb+srv://cluster0.ga86qql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  await mongoose.connect(uri, {
+    dbName,
+    user,
+    pass,
+  });
 }
