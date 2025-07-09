fad480a8b26099864c232b5ff5b29587afafaa28
Nico <aroca.twelve@proton.me>
2025-07-09 19:00:40 -0400

    created the jsx files for the different pages

--- orange-bytes/src/components/Navbar.jsx
+++ orange-bytes/src/components/Navbar.jsx
@@ -3,9 +3,36 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-      <h1>hello</h1>
+      <nav className='sticky top-0 w-full bg-blue-400 text-black h-[8rem] z-50'>
+        <div className='flex items-center h-full w-full mx-auto px-5'>
+          {/* LEFT */} 
+          <div className="ml-[3rem]">
+            <h1 className='text-3xl'>
+              <Link to="/">
+                EventXchange
+              </Link>
+            </h1>
+          </div>
+          {/*  SPACER  */}
+          <div className="flex-grow"></div>
+
+          {/* RIGHT (desktop) */}
+          <div className="mr-[3rem]">
+            <ul className="hidden sm:flex space-x-6 font-bold text-xl font-hti font">
+              <li className="transition-transform duration-50 hover:scale-102"><Link to="/dashboard">Dashboard</Link></li>
+              <li className="transition-transform duration-50 hover:scale-102"><Link to="/events">Events</Link></li>
+              <li className="transition-transform duration-50 hover:scale-102"><Link to="/calender">Calender</Link></li>
+            </ul>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
