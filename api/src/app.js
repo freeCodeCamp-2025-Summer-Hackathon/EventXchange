49dc83bd51c97c3939a11e5f5df16398e7065dfa
Brett Kinnamon <bdkinna@gmail.com>
2025-07-11 13:53:38 -0400

    feat: add session management middleware

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,14 @@
+import bodyParser from 'body-parser';
 import 'dotenv/config';
 import express from 'express';
 import {dbConnect} from './models/db.js';
-import bodyParser from 'body-parser';
 import {usersRouter} from './routes/user.routes.js';
+import {setupSession} from './utils/setupSession.js';
+import session from 'express-session';
 
+const port = 3000;
+
+// Attempt to connect to the database
 try {
   await dbConnect();
 } catch (error) {
@@ -11,17 +16,20 @@ try {
   process.exit(1);
 }
 
-const port = 3000;
-
+// Setup the express server
 const app = express();
 app.use(bodyParser.json());
+app.use(await setupSession());
 
 app.get('/', (req, res) => {
+  req.session.thing = 'TEST';
   res.send('Hello World!');
 });
 
+// Add the routes
 app.use('/api/v1', usersRouter);
 
+// Run the server
 app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`);
 });
