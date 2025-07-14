c3bc420ac43946608ccc7df34e7758918f69aabc
KeithVM <keithmiquela@gmail.com>
2025-07-14 13:33:33 -0400

    changed home to landing

--- orange-bytes/src/routes/Routes.jsx
+++ orange-bytes/src/routes/Routes.jsx
@@ -4,14 +4,14 @@ import React from "react";
 // import page jsx files
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import Home from "../pages/Home";
+import Landing from "../pages/Landing";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {path: "", element: <Home />},
+      {path: "", element: <Landing />},
       { path: "login", element: <Login /> },
       { path: "signup", element: <Signup /> },
       // {path: "*", element: <PageNotFound />},
