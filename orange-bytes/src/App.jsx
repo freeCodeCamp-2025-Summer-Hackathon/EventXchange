ade928a2537ba5846a61bd9bcc6678d78c34a757
Brett Kinnamon <bdkinna@gmail.com>
2025-07-15 21:12:01 -0400

    feat: add user login code to frontend

--- orange-bytes/src/App.jsx
+++ orange-bytes/src/App.jsx
@@ -1,10 +1,12 @@
-import Navbar from './components/Navbar'
-import { Outlet } from 'react-router-dom'
-import ScrollToTop from './components/ScrollToTop'
-import { useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { createContext, useEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+
+export const UserContext = createContext()
 
 const App = () => {
+  const [user, setUser] = useState();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -18,13 +20,15 @@ const App = () => {
   }, [navigate, location]);
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar/>
-      <ScrollToTop />
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-    </div>
+    <UserContext.Provider value={[user, setUser]}>
+      <div className="flex flex-col min-h-screen">
+        <Navbar/>
+        <ScrollToTop />
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+      </div>
+    </UserContext.Provider>
   )
 }
 
--- orange-bytes/src/pages/Login.jsx
+++ orange-bytes/src/pages/Login.jsx
@@ -1,9 +1,42 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../App.jsx';
+
 const Login = () => {
+    const [_user, setUser] = useContext(UserContext);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleInput = (setFn, event) => {
+        setFn(event.currentTarget.value);
+    }
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        if (!password || !username) return;
+        const u = fetch('http://localhost:3000/api/v1/login', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        })
+
+        if (u) {
+            setUser(u)
+            // TODO: use a router parameter to determine where to redirect to
+            // while using "/" as a fallback or default.
+            redirect("/")
+        } else {
+            // TODO: an error happened so we should tell the user
+            alert('Invalid credentials. Try again.')
+        }
+    };
+
     return (
         <div className="flex items-center justify-center h-[calc(100vh-6rem)] bg-gray-100 ">
             <div className="bg-white p-8 rounded shadow-md w-96">
                 <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={(event) => handleLogin(event)}>
                     <div className="mb-4">
                         <label className="block text-sm font-medium mb-2" htmlFor="email">
                             Email
@@ -13,6 +46,8 @@ const Login = () => {
                             type="email"
                             id="email"
                             required
+                            value={username}
+                            onInput={(event) => handleInput(setUsername, event)}
                         />
                     </div>
                     <div className="mb-6">
@@ -24,6 +59,8 @@ const Login = () => {
                             type="password"
                             id="password"
                             required
+                            value={password}
+                            onInput={(event) => handleInput(setPassword, event)}
                         />
                     </div>
                     <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 cursor-pointer w-full" type="submit">
