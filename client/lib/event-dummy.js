65aaa28b5ac350574526deb173409952b75f75ae
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-17 15:25:04 -0400

    Create event and user dummy data

---
+++ client/lib/event-dummy.js
@@ -0,0 +1,42 @@
+[
+    {
+        id: 1,
+        title: "Grand Cake Bakefest",
+        desc: "Bake, eat, and chat all day with friends!",
+        start: ["08/17/2025", "10:00 AM"],
+        location: "Boston",
+        organizer: "john123",
+        attendees: ["jane456", "jack789", "jesse1011"],
+        tags: ["food", "culinary", "cozy", "patissier"]
+    },
+    {
+        id: 2,
+        title: "Storybook Marathon",
+        desc: "Share your family's favourite literary hits in a round robin!",
+        start: ["04/21/2025", "1:00 PM"],
+        location: "Seattle",
+        organizer: "jack789",
+        attendees: ["john123", "jane456", "jesse1011"],
+        tags: ["family", "fiction", "reading"]
+    },
+    {
+        id: 3,
+        title: "Arts and Crafts Exhibition",
+        desc: "Show the world what your creativity's made of!",
+        start: ["09/07/2025", "6:00 PM"],
+        location: "California",
+        organizer: "jesse1011",
+        attendees: ["john123", "jane456"],
+        tags: ["all-ages", "showcase", "hands-on", "cozy"]
+    },
+    {
+        id: 4,
+        title: "Robotics Labs Competition",
+        desc: "Challenge yourself solo or with friends to build a robot in 8 hours!",
+        start: ["10/14/2025", "9:00 AM"],
+        location: "Atlanta",
+        organizer: "jane456",
+        attendees: ["john123", "jack879", "jesse1011"],
+        tags: ["family", "all-ages", "hands-on"]
+    },
+]
---
+++ client/lib/user-dummy.js
@@ -0,0 +1,31 @@
+[
+    {
+        id: 1,
+        fullname: "John Doe",
+        username: "john123",
+        email: "john.doe@email.com",
+        password: "strongPasswordThisIs87",
+    },
+    {
+        id: 2,
+        fullname: "Jane Doe",
+        username: "jane456",
+        email: "jane.doe@email.com",
+        password: "alsoVeryStrongPasswordThisIs42",
+    }
+    ,
+    {
+        id: 3,
+        fullname: "Jack Doe",
+        username: "jack789",
+        email: "jack.doe@email.com",
+        password: "anotherStrongPasswordThisIs96",
+    },
+    {
+        id: 4,
+        fullname: "Jesse Doe",
+        username: "jesse1011",
+        email: "jesse.doe@email.com",
+        password: "ohoWhatStrongPasswordThisIs35",
+    }
+]
