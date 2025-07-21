240642911ae8aa41d925b02a328a42323ce4a526
Brett Kinnamon <bdkinna@gmail.com>
2025-07-21 18:26:44 -0400

    fix: use db uri in .env file

--- server/src/utils/dbUri.js
+++ server/src/utils/dbUri.js
@@ -1,7 +1,4 @@
 export function dbUri() {
-  const dbName = process.env.DB_NAME;
-  const user = process.env.DB_USER;
-  const pass = process.env.DB_PASS;
-  const uri = `mongodb+srv://${user}:${pass}@cluster0.ga86qql.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+  const uri = process.env.DB_URI;
   return uri;
 }
