ed5475850045d05b9ef41f7b9a28f06c96f2c967
tailsmonster <aroca.twelve@proton.me>
2025-07-22 14:28:34 -0400

    fixed minor stuff

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="sticky top-0 w-full bg-blue-400 text-black h-[6rem] z-50">
+      <nav className="sticky top-0 w-full bg-darkTangerine text-black font-chocoSans h-[6rem] z-50">
         <div className="flex items-center h-full w-full mx-auto px-5">
           {/* LEFT */}
           <div className="ml-[3rem]">
@@ -23,9 +23,9 @@ const Navbar = () => {
           {/* RIGHT (desktop) */}
           <div className="mr-[3rem]">
             <ul className="hidden sm:flex space-x-6 font-bold text-xl font-hti font">
-              <li className="transition-transform duration-50 hover:scale-102">
+              {/* <li className="transition-transform duration-50 hover:scale-102">
                 <Link to="/dashboard">Dashboard</Link>
-              </li>
+              </li> */}
               <li className="transition-transform duration-50 hover:scale-102">
                 <Link to="/events">Events</Link>
               </li>
