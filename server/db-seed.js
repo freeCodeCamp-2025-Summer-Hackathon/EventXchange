52f0de52ee2c61e8217f3c2d6075d23c856ed309
Brett Kinnamon <bdkinna@gmail.com>
2025-07-23 15:19:30 -0400

    chore: add database seed script
    Run using `npm run seed` in the root or server directory.

---
+++ server/db-seed.js
@@ -0,0 +1,64 @@
+import {faker} from '@faker-js/faker';
+import 'dotenv/config';
+import {createEvent} from './src/controllers/event.controller.js';
+import {createUser} from './src/controllers/user.controller.js';
+import {dbConnect, dbDisconnect} from './src/models/db.js';
+
+const numberOfUsers = 5;
+const numberOfEvents = 5;
+const maxNumberOfTags = 3;
+
+seedDatabase();
+
+async function seedDatabase() {
+  await dbConnect();
+
+  const userIds = [];
+  for (let i = 0; i < numberOfUsers; i++) {
+    const u = await createRandomUser();
+    userIds.push(u.id);
+  }
+
+  for (let i = 0; i < numberOfEvents; i++) {
+    await createRandomEvent(userIds);
+  }
+
+  await dbDisconnect();
+}
+
+async function createRandomUser() {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const name = `${firstName} ${lastName}`;
+  const username = faker.internet.username({firstName, lastName});
+  const password = faker.internet.password();
+  return await createUser(name, username, password);
+}
+
+async function createRandomEvent(userIds) {
+  const title = faker.music.album();
+  const description = faker.food.description();
+  const createdAt = faker.date.past();
+  const start_date = faker.date.future();
+  const location = faker.location.streetAddress({useFullAddress: true});
+  const organizer = faker.helpers.arrayElement(userIds);
+  const attendees = faker.helpers.arrayElements(
+    userIds.filter(id => id !== organizer),
+    {min: 0, max: numberOfUsers - 2},
+  );
+  const tags = faker.helpers.multiple(faker.word.adjective, {
+    min: 0,
+    max: maxNumberOfTags,
+  });
+
+  await createEvent({
+    title,
+    description,
+    createdAt,
+    start_date,
+    location,
+    organizer,
+    attendees,
+    tags,
+  });
+}
