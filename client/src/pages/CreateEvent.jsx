45a42c375d097350c09494ac75deb265c6fcf21d
Brett Kinnamon <bdkinna@gmail.com>
2025-07-26 13:39:37 -0400

    feat: get the  key working for create events

--- client/src/pages/CreateEvent.jsx
+++ client/src/pages/CreateEvent.jsx
@@ -29,6 +29,9 @@ const CreateEvent = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
+    if (formData.get("online") != null) {
+      formData.set("online", true);
+    }
     const newEventOrError = await createEvent(formData);
     if (newEventOrError?.error != null) {
       // TODO: do something with the error
