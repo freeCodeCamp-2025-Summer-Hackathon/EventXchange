c0b39edac30bc031038b3498f10dd3baa824cfc3
Brett Kinnamon <bdkinna@gmail.com>
2025-07-26 13:12:39 -0400

    feat(client): add get event function to event service

--- client/src/services/eventService.js
+++ client/src/services/eventService.js
@@ -8,3 +8,24 @@ export const createEvent = async (eventData) => {
   }).then((r) => r.json());
   return response;
 };
+
+/**
+ * Gets a single event from the api
+ * @param {string} id
+ */
+export const getEvent = async (id) => {
+  const response = await fetch(`${apiBaseUrl}/events/${id}`, {
+    credentials: "include",
+  }).then((r) => r.json());
+  return response;
+};
+
+// export const updateEvent = async (id, eventData) => {
+//   eventData.
+//   const response = await fetch(apiBaseUrl + "/events", {
+//     method: "PUT",
+//     credentials: "include",
+//     body: eventData,
+//   }).then((r) => r.json());
+//   return response;
+// };
