0eee810870b9ebb046d23b01ff9539778af10b67
Brett Kinnamon <bdkinna@gmail.com>
2025-07-26 21:30:33 -0400

    feat: add data to the calendar

--- client/src/pages/Calender.jsx
+++ client/src/pages/Calender.jsx
@@ -1,7 +1,5 @@
 // Imported plugins for ScheduleXCalendar and plugins
 // docs from ScheduleXCalendar: https://schedule-x.dev/docs/frameworks/react
-import React, { useState } from "react";
-import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
 import {
   createViewDay,
   createViewMonthAgenda,
@@ -9,12 +7,14 @@ import {
   createViewWeek,
 } from "@schedule-x/calendar";
 import { createEventsServicePlugin } from "@schedule-x/events-service";
+import { ScheduleXCalendar, useCalendarApp } from "@schedule-x/react";
+import { useState } from "react";
 
-import "@schedule-x/theme-default/dist/index.css";
-import { useEffect } from "react";
 import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
 import { createEventModalPlugin } from "@schedule-x/event-modal";
-import eventDummy from "../../lib/event-dummy.js";
+import "@schedule-x/theme-default/dist/index.css";
+import { useEffect } from "react";
+import { getAllEvents } from "../services/eventService";
 
 function CalendarApp() {
   const eventsService = useState(() => createEventsServicePlugin())[0];
@@ -27,16 +27,6 @@ function CalendarApp() {
       createViewMonthGrid(),
       createViewMonthAgenda(),
     ],
-    // 'events' are used to create an Event
-    events: [
-      {
-        id: "1",
-        title: "First 1",
-        start: "2025-07-28 08:00",
-        end: "2025-07-28 17:00",
-        description: "Really cool event",
-      },
-    ],
 
     plugins: [
       eventsService,
@@ -45,20 +35,52 @@ function CalendarApp() {
     ],
   });
 
+  const fixDatetime = (datetime) => {
+    const date = new Date(datetime)
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    return `${year}-${month}-${day} ${hour}:${minute}`;
+  };
+
+  const fetchEvents = async () => {
+    const response = await getAllEvents();
+    if (response.error != null) {
+      // TODO: notify the user
+      console.error(response.error);
+      setEvents([]);
+      calendar.eventsService.set([]);
+      return;
+    }
+    calendar?.eventsService.set(
+      response.map((event) => {
+        const start = fixDatetime(event.start);
+        const end = fixDatetime(event.end);
+        return {
+          id: event.id,
+          title: event.title,
+          start,
+          end,
+        };
+      })
+    );
+  };
+
   useEffect(() => {
-    // get all events
-    eventsService.getAll();
-  });
+    fetchEvents();
+  }, [calendar]);
 
   return (
     <>
       {
         // add Chocolate Sans in the future
       }
-      <h1 class="text-center center text-4xl text-orange-600 py-10">
+      <h1 className="text-center center text-4xl text-orange-600 py-10">
         Events Calendar
       </h1>
-      <div class="font-(Chocolate-Classical-Sans) pt-15px max-w-90vw h-100 mx-15">
+      <div className="font-(Chocolate-Classical-Sans) pt-15px max-w-90vw h-[60vh] overflow-y-auto mx-15">
         <ScheduleXCalendar calendarApp={calendar} />
       </div>
     </>
