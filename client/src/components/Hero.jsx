56d1d9092323db971acdac871ebdefc88b4b80d5
Nico <aroca.twelve@proton.me>
2025-07-28 20:07:08 -0400

    updated homepage, fixed add event button

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { UserContext } from "../App"
 
 const Hero = () => {
+    const [user] = useContext(UserContext);
 
     return (
         <section className="bg-gradient-to-b from-orange-50 to-white py-20 px-6 text-center mb-[-3rem]">
@@ -12,7 +15,11 @@ const Hero = () => {
                         Your one-stop platform for discovering and sharing events in your community.
                 </p>
                 <div className="flex justify-center gap-4">
-                    <Link className="bg-darkTangerine text-white px-6 py-3 rounded-lg text-lg hover:bg-orange-700 transition cursor-pointer" to="/signup">Get Started</Link>
+                    {user && (
+                        <Link className="bg-darkTangerine text-white px-6 py-3 rounded-lg text-lg hover:bg-orange-700 transition cursor-pointer" to="/signup">Get Started</Link>
+
+                    )
+                }
                     <Link className="border border-gray-400 px-6 py-3 rounded-lg text-lg hover:bg-gray-100 transition cursor-pointer" to="/about">Learn More</Link>
                 </div>
             </div>
